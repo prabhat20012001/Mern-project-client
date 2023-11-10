a348041f2f975cdbe2edc415d4c9ef595a9ec7af
Prabhat kumar <prabhatkumarmnnit@gmail.com>
2023-11-11 01:34:00 +0530

    all blogs fetched on frontend

--- vite-project/src/pages/AddBlog.jsx
+++ vite-project/src/pages/AddBlog.jsx
@@ -1,9 +1,71 @@
-
-
-import React from 'react'
+import React from 'react';
 
 export default function AddBlog() {
   return (
-    <div>AddBlog</div>
-  )
+    <>
+      <div className="flex flex-wrap ">
+        <div className="w-full lg:w-full my-6 pr-0 lg:pr-2">
+          <p className="text-xl pb-6 flex items-center ">
+            <i className="fas fa-list mr-3"></i> Add Blog
+          </p>
+          <div className="leading-loose">
+            <form className="p-10 bg-white rounded shadow-xl">
+              <div className="">
+                <label className="block text-sm text-gray-600" htmlFor="name"> Title
+                </label>
+                <input
+                  className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                  id="name"
+                  name="name"
+                  type="text"
+                  required=""
+                  placeholder="Add Blog Title"
+                  aria-label="Name"
+                />
+              </div>
+              <div className="mt-2">
+                <label className="block text-sm text-gray-600" htmlFor="email">
+                  Description
+                </label>
+                <input
+                  className="w-full px-5 py-4 text-gray-700 bg-gray-200 rounded"
+                  id="email"
+                  name="email"
+                  type="text"
+                  required=""
+                  placeholder="Add Blog Description"
+                  aria-label="Email"
+                />
+              </div>
+              <div className="mt-2">
+                <label
+                  className="block text-sm text-gray-600"
+                  htmlFor="message"
+                >
+                  ImgUrl
+                </label>
+                <textarea
+                  className="w-full px-5 py-2 text-gray-700 bg-gray-200 rounded"
+                  id="message"
+                  name="message"
+                  rows="6"
+                  required=""
+                  placeholder="Add Blog ImgUrl.."
+                  aria-label="Email"
+                ></textarea>
+              </div>
+              <div className="mt-6 ">
+                <button
+                  className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
+                  type="submit"
+                >
+                  Add Blog
+                </button> 
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get(
+          'https://mern-project1-j9pi.onrender.com/api/blogs/allblogs',
+          {
+            withCredentials: true,
+          }
+        );
+        setBlogs(response.data.blogs);
+      } catch (error) {
+        console.error('Error fetching blogs:', error);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <>
+    <div className='flex text-center justify-center my-4'>
+    <h1 className='font-bold text-3xl'>All blogs</h1>
+
+    </div>
+      {blogs.map((blog) => (
+        <div key={blog._id} className="card lg:card-side bg-base-100 shadow-xl shadow-slate-500 my-4 ">
+          <figure>
+            <img
+              src={blog.imageUrl}
+              alt={`Image for ${blog.title}`}
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{blog.title}</h2>
+            <p>{blog.description}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Read More</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
